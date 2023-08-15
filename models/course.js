21dceabb56889524936e82d19c104b45555727da
a.sotnichenko <haliivi@mail.ru>
2023-08-15 08:21:17 +0700

    1) Added static method update for model Course. 2) Register get router for edit course and post router for save it. 3) Added button for edit course. 4) Write edit page. 5) Refactoring main template.

--- models/course.js
+++ models/course.js
@@ -51,6 +51,22 @@ class Course {
         const courses = await Course.getAll()
         return courses.find(c => c.id === id)
     }
+
+    static async update(course) {
+        const courses = await Course.getAll()
+        const idx = courses.findIndex(c => c.id === course.id)
+        courses[idx] = course
+        return new Promise((resolve, reject) => {
+            fs.writeFile(
+                path.join(__dirname, '..', 'data', 'courses.json'),
+                JSON.stringify(courses),
+                (err) => {
+                    if (err) reject(err)
+                    resolve()
+                }
+            )
+        })
+    }
 }
 
 module.exports = Course
