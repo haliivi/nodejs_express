796ebb343ad885d990d59d4f8f2ca5c9ab01912d
a.sotnichenko <haliivi@mail.ru>
2023-08-16 11:51:59 +0700

    1) Styling course form. 2) Register remove router. 3) Refactoring course router. 4) Added delete button to edit form course.

--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,11 @@
 const {Router} = require('express')
-const Course = require('../models/JSON/course')
+// const Course = require('../models/JSON/course')
+const Course = require('../models/MongoDB/course')
 const router = Router()
 
 router.get('/', async (req, res) => {
-    const courses = await Course.getAll()
+    // const courses = await Course.getAll()
+    const courses = await Course.find().lean()
     res.render(
         'courses',
         {
@@ -18,7 +20,8 @@ router.get('/:id/edit', async (req, res) => {
     if (!req.query.allow) {
         return res.redirect('/')
     }
-    const course = await Course.getById(req.params.id)
+    // const course = await Course.getById(req.params.id)
+    const course = await Course.findById(req.params.id).lean()
     res.render(
         'edit',
         {
@@ -29,12 +32,16 @@ router.get('/:id/edit', async (req, res) => {
 })
 
 router.post('/edit', async (req, res) => {
-    await Course.update(req.body)
+    // await Course.update(req.body)
+    const {id} = req.body
+    delete req.body.id
+    await Course.findByIdAndUpdate(id, req.body)
     res.redirect('/courses')
 })
 
 router.get('/:id', async (req, res) => {
-    const course = await Course.getById(req.params.id)
+    // const course = await Course.getById(req.params.id)
+    const course = await Course.findById(req.params.id).lean()
     res.render(
         'course',
         {
@@ -43,6 +50,16 @@ router.get('/:id', async (req, res) => {
             course
         }
     )
+})
+
+router.post('/remove', async (req, res) => {
+    try {
+        await Course.deleteOne({_id: req.body.id})
+        res.redirect('/courses')
+    } catch (e) {
+        console.log(e)
+    }
+
 })
 
 module.exports = router
