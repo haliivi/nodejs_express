e20bc5e32bb1e57fca856a4dabdec986c447009e
a.sotnichenko <haliivi@mail.ru>
2023-08-16 13:12:41 +0700

    Create model User.

---
+++ models/MongoDB/user.js
@@ -0,0 +1,30 @@
+const {Schema, model} = require('mongoose')
+
+const user = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    card: {
+        items: [
+            {
+                count: {
+                    type: Number,
+                    required: true,
+                    default: 1
+                },
+                courseId: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'Course',
+                    required: true,
+                }
+            }
+        ]
+    }
+})
+
+module.exports = model('User', user)
