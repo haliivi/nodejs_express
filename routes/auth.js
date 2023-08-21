3bb707fe10d4b90d38e40cb5530e1315a261fde5
a.sotnichenko <haliivi@mail.ru>
2023-08-21 10:55:22 +0700

    1) Install and plug in express-validator. 2) Write validators.js for register. 3) Turn off built-in form validation. 4) Plug in register validation.

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const {Router} = require('express')
+const {validationResult} = require('express-validator')
 const User = require('../models/MongoDB/user')
 const bcrypt = require('bcryptjs')
 const crypto = require('crypto')
 const nodemailer = require('nodemailer')
 const registrationMail = require('../emails/registration')
 const resetMail = require('../emails/reset')
+const {registerValidators} = require('../utils/validators')
 const router = Router()
 
 const transport = nodemailer.createTransport({
@@ -54,10 +56,15 @@ router.post('/login', async (req, res) => {
     }
 })
 
-router.post('/register', async (req, res) => {
+router.post('/register', registerValidators, async (req, res) => {
     try {
         const {email, password, repeat, name} = req.body
         const candidate = await User.findOne({email})
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            req.flash('registerError', errors.array()[0].msg)
+            return res.status(422).redirect('/auth/login#register')
+        }
         if (candidate) {
             req.flash('registerError', 'Пользователь с таким email уже существует')
             res.redirect('/auth/login#register')
